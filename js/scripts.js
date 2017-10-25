186d163040fce274003c66aab349328276b06e1b
Kevin Babcock and Malgorzata Haniszewska <kevin@>
2017-10-25 16:16:15 -0700

    take words that begin with consonants, remove those consonants and add them to the end, then add ay at the end

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 var pigLatin = function(string) {
   var words = string.split(" ");
-  var newStrings = words.map(function(word) {
-    if ((word.charAt(0)==="a") || (word.charAt(0)==="e") || (word.charAt(0)==="i") || (word.charAt(0)==="o") || (word.charAt(0)==="u")) {
-      return word = word.concat("way");
-    } else {
-      return word;
-    };
+  // var newStrings = words.map(function(word) {
+  //   if ((word.charAt(0)==="a") || (word.charAt(0)==="e") || (word.charAt(0)==="i") || (word.charAt(0)==="o") || (word.charAt(0)==="u")) {
+  //     var word = word.concat("way");
+  //     alert(word);
+  //     return word;
+  //   } else {
+  //     return word;
+  //   };
+  // });
 
+
+  var secondStrings = words.map(function(word) {
+    for (index=1; index < word.length; index++) {
+      if ((word.charAt(index)==="a") || (word.charAt(index)==="e") || (word.charAt(index)==="i") || (word.charAt(index)==="o") || (word.charAt(index)==="u")) {
+        
+        var firstConsonants = word.slice(0, index);
+        var lastConsonants = word.slice(index, word.length);
+        var newWord = (lastConsonants + firstConsonants + "ay");
+
+        return newWord;
+      }
+    }
   });
-  alert(newStrings);
-
-};
+}
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var userString = $("#userString").val();
     var result = pigLatin(userString);
+    //alert(userString);
   });
 });
