2cb0286ef758e7d9d839e35eaa807fbd6e1b2356
Kevin Babcock and Malgorzata Haniszewska <kevin@>
2017-10-26 09:16:35 -0700

    fix bug for words with qu

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,12 @@ var pigLatin = function(string) {
               var lastConsonants = word.slice(index + 1, word.length);
               var newWord = (lastConsonants + firstConsonants + "ay");
               return newWord;
+            } else {
+              var firstConsonants = word.slice(0, index);
+              var lastConsonants = word.slice(index, word.length);
+              var newWord = (lastConsonants + firstConsonants + "ay");
+
+              return newWord;
             }
           } else {
             var firstConsonants = word.slice(0, index);
@@ -26,38 +32,19 @@ var pigLatin = function(string) {
       }
     }
   });
-  alert(newStrings);
+  var outputString = "";
+  newStrings.forEach(function(newString) {
+    outputString = outputString.concat(newString + " ");
+  })
+  return outputString;
 };
-
-  // var thirdStrings = words.map(function(word) {
-  //   for (index=1; index < word.length; index++) {
-  //     if ((word.charAt(index)==="a") || (word.charAt(index)==="e") || (word.charAt(index)==="i") || (word.charAt(index)==="o") || (word.charAt(index)==="u") || (word.charAt(index) === "y")) {
-  //
-  //       if (word.includes("qu")) {
-  //         var firstConsonants = word.slice(0, index + 1);
-  //         if (firstConsonants.includes("qu")) {
-  //           var firstConsonants = word.slice(0, index + 1);
-  //           var lastConsonants = word.slice(index + 1, word.length);
-  //           var newWord = (lastConsonants + firstConsonants + "ay");
-  //           return newWord;
-  //         } else {
-  //           var firstConsonants = word.slice(0, index);
-  //           var lastConsonants = word.slice(index, word.length);
-  //           var newWord = (lastConsonants + firstConsonants + "ay");
-  //           return newWord;
-  //         }
-
-//         }
-//       }
-//     };
-//   })
-// }
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var userString = $("#userString").val();
     var result = pigLatin(userString);
-    //alert(pigLatin(userString));
+    $(".result").show();
+    $("#pigLatin").text(result);
   });
 });
